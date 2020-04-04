76aad706f028795a6ebb6666312cc805c4ae59e9
Piotr Hankiewicz <piotr.hankiewicz@gig.com>
2020-04-04 23:18:43 +0200

    feature: add local search greedy

---
+++ src/local_search.py
@@ -0,0 +1,66 @@
+import random
+
+import math
+from collections import defaultdict
+import itertools
+
+import numpy as np
+
+
+def calculate_cycle_length(cycle_vertices, distance_matrix):
+    return sum([
+        distance_matrix[vertex1, vertex2]
+        for vertex1, vertex2 in zip(cycle_vertices, cycle_vertices[1:])
+    ])
+
+
+def swap_edges_diff(distance_matrix, solution, index1, index2):
+    edge1_start, edge1_end = solution[index1], solution[(index1 + 1) % len(solution)]
+    edge2_start, edge2_end = solution[index2], solution[(index2 + 1) % len(solution)]
+
+    return (
+        distance_matrix[edge1_start, edge2_start]
+        + distance_matrix[edge1_end, edge2_end]
+        - distance_matrix[edge1_start, edge1_end]
+        - distance_matrix[edge2_start, edge2_end]
+    )
+
+
+def local_search_greedy(distance_matrix):
+    _, matrix_width = distance_matrix.shape
+    number_of_vertices_required = math.ceil(0.5 * matrix_width)
+
+    vertices = list(range(matrix_width))
+
+    solution = random.sample(vertices, number_of_vertices_required)
+
+    improvement = True
+
+    while improvement:
+        improvement = False
+
+        best_diff = 0
+        best_swap = []
+        for swap_index1, swap_index2 in itertools.combinations(
+            range(len(solution)),
+            2,
+        ):
+            diff = swap_edges_diff(
+                distance_matrix, solution, swap_index1, swap_index2
+            )
+
+            if diff < best_diff:
+                best_diff = diff
+                best_swap = [swap_index1, swap_index2]
+                improvement = True
+
+        if improvement:
+            swap_index1, swap_index2 = best_swap
+            solution = (
+                solution[:swap_index1+1]
+                + list(reversed(solution[swap_index1+1: swap_index2+1]))
+                + solution[swap_index2+1:]
+            )
+
+    solution.append(solution[0])
+    return solution, calculate_cycle_length(solution, distance_matrix)
--- src/run.py
+++ src/run.py
@@ -3,6 +3,7 @@ import math
 import os
 
 from greedy import greedy_cycle_tsp, nn_greedy_tsp, regret_1_greedy_cycle_tsp
+from local_search import local_search_greedy
 from importer import create_distance_matrix, import_vertices_coordinates
 from texttable import Texttable
 from tqdm import tqdm
@@ -63,11 +64,24 @@ def run_regret_1_greedy_cycle_tsp(distance_matrix, vertices_coordinates):
     return results
 
 
+def run_local_search_greedy(distance_matrix, vertices_coordinates):
+    results = []
+    for _ in tqdm(vertices_coordinates):
+        cycle_vertices, cycle_length = local_search_greedy(distance_matrix)
+        assert cycle_vertices[0] == cycle_vertices[-1]
+        assert len(cycle_vertices) - 1 == len(set(cycle_vertices))
+        _, distance_matrix_width = distance_matrix.shape
+        assert len(cycle_vertices) - 1 == math.ceil(0.5 * distance_matrix_width)
+        results.append((cycle_vertices, cycle_length))
+    return results
+
+
 def get_algorithms_dict():
     return {
         'nn_greedy': run_nn_gready_tsp,
         'greedy_cycle': run_greedy_cycle_tsp,
         'regret_1_greedy_cycle': run_regret_1_greedy_cycle_tsp,
+        'local_search_greedy': run_local_search_greedy,
     }
 
 
