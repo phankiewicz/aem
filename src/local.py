af7edb570a69f3d8c623cbea70f95613c884dc0f
Dombearx <domin.dober@gmail.com>
2020-04-05 12:19:41 +0200

    Add not working greedy cycle

--- src/local.py
+++ src/local.py
@@ -33,10 +33,32 @@ def local_greedy_tsp(distance_matrix, starting_vertex):
     cycle_length = 0
 
     cycle_vertices = generate_random_cycle(matrix_width, number_of_vertices_required)
+    other_vertices = [i for i in range(0, matrix_width) if i not in cycle_vertices]
 
 
     for current_vertex, next_vertex in zip(cycle_vertices[0:], cycle_vertices[1:]):
         cycle_length += distance_matrix[current_vertex, next_vertex]
+
+    #random search order
+    search_order = [i for i in range(0, len(cycle_vertices) - 1)]
+    random.shuffle(search_order)
+
+    for first_vertex_to_swap in search_order:
+        search_order2 = [i for i in range(0, len(cycle_vertices) - 1)]
+        random.shuffle(search_order2)
+
+        delta = cycle_length
+        for second_vertex_to_swap in search_order2:
+            delta -= distance_matrix[first_vertex_to_swap - 1, first_vertex_to_swap]
+            delta -= distance_matrix[first_vertex_to_swap, first_vertex_to_swap + 1]
+            delta -= distance_matrix[second_vertex_to_swap - 1, second_vertex_to_swap]
+            delta -= distance_matrix[second_vertex_to_swap, second_vertex_to_swap + 1]
+
+            delta += distance_matrix[first_vertex_to_swap - 1, second_vertex_to_swap]
+
+
+
+
 
 
     return cycle_vertices, cycle_length
